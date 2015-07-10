bb0a599e39a66fc74b761b3e0e3d89998667fc06
yamamoto-yosuke <yamamoto-yosuke@gm.janome.co.jp>
2015-07-10 10:31:32 +0900

    コメント追加 夕方判定のバグ修正

--- weather.py
+++ weather.py
@@ -4,39 +4,40 @@ import RPi.GPIO as GPIO
 import datetime
 import locale
 
-d = datetime.datetime.today()
-day = 0
-result = pywapi.get_weather_from_weather_com('JAXX6842:1:JA')
+precip = 0
 
-if d.hour > 16:
-    day = float(result['forecasts'][0]['day']['chance_precip'])/100.0
+d = datetime.datetime.today()
+weather_com_result = pywapi.get_weather_from_weather_com('JAXX6842:1:JA') #地域の指定
+
+if d.hour < 16:
+    precip = float(weather_com_result['forecasts'][0]['day']['chance_precip'])/100.0
 else:
-    day = float(result['forecasts'][0]['night']['chance_precip'])/100.0
+    precip = float(weather_com_result['forecasts'][0]['night']['chance_precip'])/100.0
 
 REDPIN = 18
 BLUEPIN = 13
 GREENPIN = 19
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) #BCMでピン番号指定
 GPIO.setup(REDPIN,GPIO.OUT)
 GPIO.setup(BLUEPIN,GPIO.OUT)
 GPIO.setup(GREENPIN,GPIO.OUT)
 
-print day
+print precip
 
-if day > 0.81:
+if precip > 0.81:
     GPIO.output(REDPIN,GPIO.LOW)
     GPIO.output(BLUEPIN,GPIO.HIGH)
     GPIO.output(GREENPIN,GPIO.HIGH)
-elif day > 0.55:
+elif precip > 0.55:
     GPIO.output(REDPIN,GPIO.LOW)
     GPIO.output(BLUEPIN,GPIO.HIGH)
     GPIO.output(GREENPIN,GPIO.LOW)
-elif day > 0.30:
+elif precip > 0.30:
     GPIO.output(REDPIN,GPIO.HIGH)
     GPIO.output(BLUEPIN,GPIO.LOW)
     GPIO.output(GREENPIN,GPIO.HIGH)
-elif day > 0.20:
+elif precip > 0.20:
     GPIO.output(REDPIN,GPIO.HIGH)
     GPIO.output(BLUEPIN,GPIO.LOW)
     GPIO.output(GREENPIN,GPIO.LOW)
